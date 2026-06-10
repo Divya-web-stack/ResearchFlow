40c8b6d443cf751164c8d287625c59052f9772c0
Divya Rewade <divyarewade2005@gmail.com>
2026-06-10 23:28:29 +0530

    final commit

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_BASE = "http://localhost:8000/api"
+API_BASE = "https://researchflow-0rqd.onrender.com"
 
 st.set_page_config(
     page_title="ResearchFlow AI",
